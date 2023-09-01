b5e27190c30d958cb992206dc5d4bbfe98bdc593
sooryaprakash31 <sooryaprakash.r31@gmail.com>
2023-09-01 23:28:29 +0530

    Added support for request params

--- flask_app/app/views/controller.py
+++ flask_app/app/views/controller.py
@@ -1,5 +1,6 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 
+from ..po.recommendation_po import RecommendationPo
 from ..services.recommendation_service import RecommendationService
 
 controller = Blueprint('controller', __name__)
@@ -7,6 +8,23 @@ controller = Blueprint('controller', __name__)
 
 @controller.route('/recommend/<string:title>/<string:year>', methods=['POST'])
 def recommend(title, year):
-    recommendations = RecommendationService(title=title, year=year).get_recommendations()
+
+    # todo: Add validation
+
+    data = validate_data(request.form.to_dict())
+
+    recommendation_po = RecommendationPo(**data)
+
+    recommendations = RecommendationService(title=title, year=year, recommendation_po=recommendation_po). \
+        get_recommendations()
 
     return jsonify(recommendations.to_dict(orient='records')), 200
+
+
+def validate_data(data):
+    return {
+        "recommendations_count": int(data["recommendations_count"]),
+        "rating_filter": float(data["rating_filter"]),
+        "popularity_threshold": float(data["popularity_threshold"])
+    }
+
