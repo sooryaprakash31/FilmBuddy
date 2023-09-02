6004140e132123eb4362b9a7307cd8d7e36331d4
sooryaprakash31 <sooryaprakash.r31@gmail.com>
2023-09-02 23:07:03 +0530

    Added validation for recommend endpoint params

--- flask_app/app/views/controller.py
+++ flask_app/app/views/controller.py
@@ -2,6 +2,7 @@ from flask import Blueprint, jsonify, request
 
 from ..po.recommendation_po import RecommendationPo
 from ..services.recommendation_service import RecommendationService
+from ..services.request_validation_service import RequestValidationService
 
 controller = Blueprint('controller', __name__)
 
@@ -9,22 +10,12 @@ controller = Blueprint('controller', __name__)
 @controller.route('/recommend/<string:title>/<string:year>', methods=['POST'])
 def recommend(title, year):
 
-    # todo: Add validation
+    valid_data = RequestValidationService(request=request).validate_data()
 
-    data = validate_data(request.form.to_dict())
-
-    recommendation_po = RecommendationPo(**data)
+    recommendation_po = RecommendationPo(**valid_data)
 
     recommendations = RecommendationService(title=title, year=year, recommendation_po=recommendation_po). \
         get_recommendations()
 
     return jsonify(recommendations.to_dict(orient='records')), 200
-
-
-def validate_data(data):
-    return {
-        "recommendations_count": int(data["recommendations_count"]),
-        "rating_filter": float(data["rating_filter"]),
-        "popularity_threshold": float(data["popularity_threshold"])
-    }
 
